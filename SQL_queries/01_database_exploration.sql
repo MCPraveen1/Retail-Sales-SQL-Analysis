cf41b91b0b520d558bb72783a622029908adf652
MCPraveen1 <pchund00bob@gmail.com>
2025-03-26 19:44:10 +0530

    Create 01_database_exploration.sql

---
+++ SQL_queries/01_database_exploration.sql
@@ -0,0 +1,11 @@
+-- Check Customers Table
+SELECT * FROM Customers;
+
+-- Check Products Table
+SELECT * FROM Products;
+
+-- Check Stores Table
+SELECT * FROM Stores;
+
+-- Check Transactions Table
+SELECT * FROM Transactions;
